3f5b624ffebbef4f1ef995bd6632791be7d6d4f3
Jono O'Shea <jono.oshea@gmail.com>
2014-04-08 15:49:27 -0700

    Plugin changes

--- client/background.js
+++ client/background.js
@@ -1,8 +1,11 @@
 
-if ( true ) {
-
-    chrome.pageAction.show();
-
+function makeRequest() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:8000/api/v1/url", false);
+  xhr.addEventListener('load', function(e) {
+    console.log(e);
+  }, false);
+  xhr.send("url=gwd.lphbs.com");
 }
 
-
+makeRequest();
